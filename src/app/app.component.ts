dea5d7b82f48fc3f5e9625648e7fda85528c65d6
unknown <manzoor1996alam@gmail.com>
2019-02-25 18:50:24 +0530

    forms validation by using ReactiveFormsModule  @angular/forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms'
+
+import {Validators} from '@angular/forms'
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular7tutorial';
+  emailid;
+  
+  validation_messages = {
+    'emailid': [
+      { type: 'required', message: 'Email is required.' },
+      { type: 'pattern', message: 'Please enter a valid email.' }
+    ],
+
+    'password': [
+      { type: 'required', message: 'Password is required.' },
+      { type: 'minlength', message: 'Password must be at least 5 characters long.' }
+    ]
+  };
+
+  formdata = new FormGroup({
+    emailid: new FormControl('',
+     Validators.compose([
+       Validators.required,
+       Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
+    ])),
+    // passwd: new FormControl("", 
+    // this.passwordvalidation
+    // )
+   passwd : new FormControl('', 
+    Validators.compose([
+      Validators.minLength(5),
+      Validators.required
+    ])),
+ });
+
+
+ onClickSubmit(data) {this.emailid = data.emailid;}
+
 }
