8e0926d097a2f5eeb6d8fe2fe5a6a7e57cdf9d6d
unknown <manzoor1996alam@gmail.com>
2019-03-30 22:48:55 +0530

    Fixed 404 error occur when I refresh through the browser

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
 import {APP_BASE_HREF} from '@angular/common';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 
 
@@ -14,10 +15,14 @@ import {APP_BASE_HREF} from '@angular/common';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
+    AppRoutingModule, 
     AngularFontAwesomeModule
   ],
-  providers: [{provide: APP_BASE_HREF, useValue: '/my/app'}],
+  providers: [{provide: APP_BASE_HREF, useValue: '/my/app'},//my/app showing inside the url
+
+  //these line required to reload work
+  {provide: LocationStrategy, useClass: HashLocationStrategy}
+],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
